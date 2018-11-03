a3d507b7b1cb7be627f84ec112ec7a4b0321ce26
Ashcon Partovi <ashcon@partovi.net>
2018-11-03 16:35:23 -0700

    Add initial code

---
+++ src/sql.js
@@ -0,0 +1,93 @@
+const sql = require('promise-mysql')
+
+/**
+ * Standard SQL database that can send queries.
+ */
+class Sql {
+
+	/**
+	 * Connect to the SQL database with password authentication.
+	 *
+	 * @param {string} host Hostname or IP address of the SQL database.
+	 * @param {string} db Name of the database to access.
+	 * @param {string} user Name of the user logging in.
+	 * @param {string} pwd Password of the user logging in.
+	 */
+	constructor(host, db, user, pwd) {
+		this.fields = {}
+		this.db = sql.createPool({
+			host: host,
+			database: db,
+			user: user,
+			password: pwd,
+			connectionLimit: 5
+		})
+	}
+
+	/**
+	 * Send a query to the SQL database and await a response.
+	 *
+	 * @param {string} statement The SQL statement to query.
+	 * @returns {promise<object>} The response from the SQL database.
+	 */
+	query(statement) {
+		return this.db.query(statement)
+	}
+
+	/**
+	 * Ensure that a table is created with a set of fields.
+	 *
+	 * @example
+	 * assertTable('dogs', ['name CHAR(12) NOT NULL', 'age SMALLINT', 'PRIMARY KEY (name, age)'])
+	 *
+	 * @param {string} name Name of the table.
+	 * @param {array<string>} fields List of fields to create the table schema.
+	 * @returns {promise<array<string>} Array of field name from the table.
+	 */
+	assertTable(name, fields) {
+		return this.query(`CREATE TABLE IF NOT EXISTS ${name} (${fields.join(',')});`)
+			.then(response => this.query(`SHOW COLUMNS FROM ${name}`)
+                .then(fields => this.fields[name] = fields.map(field => field.Field)))
+	}
+
+	/**
+	 * Insert a row or multiple rows into a table.
+	 *
+	 * @example
+	 * insertRow('dogs', [['mars', 6], ['penny', 3]])
+	 *
+	 * @param {string} name Name of the table.
+	 * @param {object|array<object>} values A single row or array of rows.
+	 * @returns {promise<object>} The response from the SQL database.
+	 */
+	insertRow(name, values) {
+		values = values.map(val => Sql.normalize(val))
+		if(!(values[0] instanceof Array)) {
+			values = [values]
+		}
+		values = values.map(val => `(${val.join(',')})`)
+		return this.query(`INSERT IGNORE INTO ${name} VALUES ${values.join(',')};`)
+	}
+
+	/**
+	 * Convert an object into a SQL-friendly value.
+	 *
+	 * @param {object} Any object.
+	 * @returns {object} SQL-friendly representation of the object.
+	 */
+	static normalize(object) {
+		return Object.values(object).map(obj => {
+			if(obj == null || obj == undefined) {
+				return 'null'
+			} else if(typeof obj === 'string') {
+				return `\'${obj.replace('\'', '\'\'')}\'`
+			} else if(isNaN(obj)) {
+				return 0
+			}
+			return obj
+		})
+	}
+
+}
+
+module.exports = Sql
---
+++ src/sql_google.js
@@ -0,0 +1,178 @@
+const fs = require('fs')
+const http = require('request-promise')
+const sleep = require('sleep-promise')
+const Auth = require('google-auth-library')
+const BigQuery = require('@google-cloud/bigquery')
+const {Storage} = require('@google-cloud/storage')
+const Sql = require('./sql')
+
+/**
+ * Google-managed SQL cloud database.
+ *
+ * @see https://cloud.google.com/sql/docs
+ */
+class SqlGoogle extends Sql {
+
+	/**
+	 * Connect to the SQL database with Google and password authentication.
+	 *
+	 * @param {string} id Internal ID of the SQL instance.
+	 * @param {string} host Hostname or IP address of the SQL instance.
+	 * @param {string} db Name of the database to access, not the instance name.
+	 * @param {string} user Name of the user logging in.
+	 * @param {string} pwd Password of the user logging in.
+	 * @param {string} key Base64 encoded 'google.json' secret file.
+	 */
+	constructor(id, host, db, user, pwd, key) {
+		super(host, db, user, pwd)
+		this.id = id
+		this.name = db
+		var key = JSON.parse(Buffer.from(key, 'base64'))
+		var keyPath = `google-${id}.json`
+		fs.writeFileSync(keyPath, key)
+		this.projectId = key.project_id
+		this.client = new Auth.JWT(
+			key.client_email,
+			null,
+			key.private_key,
+			['https://www.googleapis.com/auth/sqlservice.admin',
+			 'https://www.googleapis.com/auth/cloud-platform'],
+			null
+		)
+		var config = {projectId: this.projectId, keyFilename: keyPath}
+		this.bigQuery = new BigQuery(config)
+		this.storage = new Storage(config)
+	}
+
+	/**
+	 * Send a request to Google's API with token authentication.
+	 *
+	 * @param {string} url URL of the request.
+	 * @param {object} data Optional data to be POST-ed with the request.
+	 * @returns {promise<object>} Response from the API.
+	 */
+	request(url, data = null) {
+		return this.client.authorize()
+			.then(token => http({
+				url: url,
+				json: data,
+				headers: {'content-type': 'application/json'},
+				auth: {'bearer': token.access_token},
+				method: data ? 'POST' : 'GET',
+				simple: true
+			}))
+			.then(body => {
+				if(typeof body === 'string') {
+					return JSON.parse(body)
+				} else {
+					return body
+				}
+			})
+	}
+
+	/**
+	 * Wait for a SQL operation to finish.
+	 *
+	 * @param {string} operationId ID of the SQL operation to wait for.
+	 * @param {integer} seconds Number of seconds to wait if not yet done.
+	 * @returns {promise} Promise when the operation successfully completed.
+	 */
+	wait(operationId, seconds = 10) {
+		return this.request(`https://www.googleapis.com/sql/v1beta4/projects/${this.projectId}/operations/${operationId}`)
+			.then(operation => {
+				if(operation.error) {
+					throw new Error(operation.error.errors[0].message)
+				} else if(operation.endTime) {
+					return operation
+				} else {
+					return sleep(seconds * 1000).then(done => this.wait(operationId, 2 * seconds))
+				}
+			})	
+	}
+
+	/**
+	 * Get or create a Google BigQuery dataset.
+	 *
+	 * @param {string} name Name of the dataset.
+	 * @returns {promise<object>} BigQuery dataset.
+	 */
+	getDataset(name) {
+		return this.bigQuery.dataset(this.name).get({autoCreate: true})
+			.then(data => data[0].table(name).get({autoCreate: true}))
+			.then(data => data[0])
+	}
+
+	/**
+	 * Get or create a Google Storage bucket using the name of the SQL database.
+	 *
+	 * @returns {promise<object>} Storage bucket.
+	 */
+	getBucket() {
+		return this.storage.bucket(this.name).get({autoCreate: true})
+			.then(data => data[0])
+	}
+
+	/**
+	 * Export a SQL table to a CSV file in the Google Storage bucket.
+	 *
+	 * @param {string} table Name of the table to export.
+	 * @param {string} file Name of the exported CSV file.
+	 * @returns {promise} Promise when the operation successfully finished.
+	 */
+	exportToCsv(table, file) {
+		return this.request(`https://www.googleapis.com/sql/v1beta4/projects/${this.projectId}/instances/${this.id}`)
+			.then(sql => this.getBucket()
+				.then(bucket => bucket.acl.update({
+					entity: `user-${sql.serviceAccountEmailAddress}`,
+					role: 'WRITER'
+				})))
+			.then(ok => this.request(
+				`https://www.googleapis.com/sql/v1beta4/projects/${this.projectId}/instances/${this.id}/export`,
+				{
+					exportContext: {
+						fileType: 'CSV',
+						uri: `gs://${this.name}/${file}.csv`,
+						databases: [this.name],
+						csvExportOptions: {
+							selectQuery: [
+								// #assertTable(name, fields) must have been called for fields to exist.
+								`SELECT '${this.fields[table].join("','")}'`,
+								`UNION ALL`,
+								`SELECT * FROM ${table}`
+							].join("\n")
+						}
+					}
+				}
+			))
+			.then(operation => this.wait(operation.name))
+	}
+
+	/**
+	 * Export a SQL table to a Google BigQuery dataset.
+	 *
+	 * @param {string} table Name of the table to export.
+	 * @param {string} file Name of the exported CSV file.
+	 * @returns {promise} Promise when the operation successfully finished.
+	 */
+	exportToBigQuery(table, file) {
+		return this.exportToCsv(table, file)
+			.then(ok => this.getDataset(table)
+				.then(table => this.getBucket()
+					.then(bucket => table.load(
+						bucket.file(`${file}.csv`),
+						{
+							autodetect: true,
+							maxBadRecords: 100,
+							sourceFormat: 'CSV',
+							writeDisposition: 'WRITE_TRUNCATE',
+							nullMarker: 'null',
+							skipLeadingRows: 1
+						}
+					))
+				))
+			.then(data => data[0])
+	}
+
+}
+
+module.exports = SqlGoogle
